97fff93c74eb62f2fffd0d6273d32d0aa51bd9bb
dmcbrayer <dmcbrayer@gmail.com>
2015-04-25 17:32:39 -0400

    Added static pages controller and views.

---
+++ app/controllers/static_pages_controller.rb
@@ -0,0 +1,11 @@
+class StaticPagesController < ApplicationController
+
+  def home
+    #This will serve as our root_url for now, until Posts#index is done
+  end
+
+  def about
+    #this will just have some basic information about the site.
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
   mount Upmin::Engine => '/admin'
   devise_for :users
+
+  root to: 'static_pages#home'
+
+  get '/about', to: 'static_pages#about'
 end
